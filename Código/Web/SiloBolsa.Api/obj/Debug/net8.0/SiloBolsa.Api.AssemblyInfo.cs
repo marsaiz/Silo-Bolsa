115371eb050b42a3b6957d429a4fe94f7a1b8c97
marsaiz <48637197+marsaiz@users.noreply.github.com>
2024-08-27 15:33:03 -0300

    .

--- Código/Web/SiloBolsa.Api/obj/Debug/net8.0/SiloBolsa.Api.AssemblyInfo.cs
+++ Código/Web/SiloBolsa.Api/obj/Debug/net8.0/SiloBolsa.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiloBolsa.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d5351121c3d897217a2c4d727edb169e030f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e5ec8692c1e2620be3f1f88d9e9886a4563aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiloBolsa.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiloBolsa.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
