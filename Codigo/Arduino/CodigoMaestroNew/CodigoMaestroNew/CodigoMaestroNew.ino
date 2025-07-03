92825eebc95ac4b3636b39271ed0b79fa414ab19
marsaiz <48637197+marsaiz@users.noreply.github.com>
2025-07-03 19:47:06 -0300

    update CodigoMaestroNew.ino

--- Codigo/Arduino/CodigoMaestroNew/CodigoMaestroNew/CodigoMaestroNew.ino
+++ Codigo/Arduino/CodigoMaestroNew/CodigoMaestroNew/CodigoMaestroNew.ino
@@ -11,9 +11,16 @@
 
 #define ssid "SAIZ"
 #define password "25768755"
+//#define ssid "Invitados"
+//#define password "holacitia"
+
+//#define ssid "laboratorio"
+//#define password "laboratorio"
+
 //#define serverName "http://192.168.1.21:5006/api/lecturas"
 //#define serverName "http://77.81.230.76:5096/api/lecturas"
-#define serverName "https://remarkable-healing-production.up.railway.app/api/lecturas";
+#define serverName "https://remarkable-healing-production.up.railway.app/api/lecturas"
+
 #define idCaja "cac70d5d-4df3-451f-bba9-59bcea039425"
 
 String getISO8601Time();
@@ -26,13 +33,13 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", -10800, 60000);//Ajuste para UTC-3 
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
 // Timer set to 30 minutes (1800000)
-unsigned long timerDelay = 1800000;
+unsigned long timerDelay = 300000;
 
 #define PIN_MQ135 A0
 
 Adafruit_AHT10 aht;
 
-//MQ135 mq135_sensor(PIN_MQ135);
+
 const int gasSensor = 0;
 float voltage = 0.00;
 float resistencia = 0.00;
@@ -45,24 +52,21 @@ RunningMedian medianFilterCO2(5); // Ventana de 5 muestras
 //ppm = 116.6020682 (Rs/Ro)^-2.769034857
 
 double a = 116.6020682;
-double a1 = 56.0820;
 
 //el exponente es negativo pero para calcular usando Pow hago el reciproco
 //de la potencia positiva,
 double b = 2.769034857;
-//double b1 = 5.9603;
-double Ro = 29619.36495;
-//double Ro1 = 34037.18774;
+double Ro= 33710.79934; 
 
 void setup() {
   Serial.begin(115200);
 
-  if (! aht.begin()) {
-    Serial.println("Could not find AHT10? Check wiring");
-    while (1) delay(10);
-  }
-  Serial.println("AHT10 found");
-  Serial.println("Voltage, Resistencia, CO2 PPM, RH, Temp");
+//  if (! aht.begin()) {
+//    Serial.println("Could not find AHT10? Check wiring");
+//    while (1) delay(10);
+//  }
+//  Serial.println("AHT10 found");
+//  Serial.println("Voltage, Resistencia, CO2 PPM, RH, Temp");
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -91,40 +95,41 @@ void enviarLectura(){
     String isoTime = getISO8601Time();
     Serial.print(F("Fecha y hora actual: ")); Serial.println(isoTime);
 
-    /* sensors_event_t humidity, temp;
-    aht.getEvent(&humidity, &temp);
+//    sensors_event_t humidity, temp;
+//    aht.getEvent(&humidity, &temp);
+//
+//    voltage = getVoltage(PIN_MQ135);
+//    resistencia = 1000.0 * ((5.0 - voltage) / voltage);
+//    dioxidoDeCarbono = a * ( 1.0 / pow( (resistencia / Ro), b)); //Obtengo valor en ppm
+//    dioxidoDeCarbono /= 10000; //Convierto a %
+//   
+//
+//    // Aplicar el filtro de mediana en la lectura de CO₂
+//    medianFilterCO2.add(dioxidoDeCarbono);  // Leer valor filtrado
+//
+//    float co2Filtrado = medianFilterCO2.getMedian();
+//    
+//    Serial.print(voltage, 5);
+//    Serial.print(";");
+//    Serial.print(resistencia, 5);
+//    Serial.print(";");
+//    Serial.print(co2Filtrado, 5);
+//    Serial.print(";");
+//    Serial.print(humidity.relative_humidity, 5);
+//    Serial.print(";");
+//    Serial.println(temp.temperature, 5);
+//
+//    //Crear un objeto JSON
+//    JsonDocument doc;
+//    doc["fechaHoraLectura"] = isoTime;
+//    doc["temp"] = temp.temperature;  // Asignar la temperatura
+//    doc["humedad"] = humidity.relative_humidity;  // Asignar la humedad
+//    doc["dioxidoDeCarbono"] = dioxidoDeCarbono;// Asignar el valor de CO2 en %
+//    doc["idCaja"] = idCaja;
 
-    voltage = getVoltage(PIN_MQ135);
-    resistencia = 1000.0 * ((5.0 - voltage) / voltage);
-    dioxidoDeCarbono = a * ( 1.0 / pow( (resistencia / Ro), b));
-    //dioxidoDeCarbono1 = a1 * ( 1.0 / pow( (resistencia / Ro1), b1));
-
-    // Aplicar el filtro de mediana en la lectura de CO₂
-    medianFilterCO2.add(dioxidoDeCarbono);  // Leer valor filtrado
-
-    float co2Filtrado = medianFilterCO2.getMedian();
-    
-    Serial.print(voltage, 5);
-    Serial.print(";");
-    Serial.print(resistencia, 5);
-    Serial.print(";");
-    Serial.print(co2Filtrado, 5);
-    Serial.print(";");
-    Serial.print(humidity.relative_humidity, 5);
-    Serial.print(";");
-    Serial.println(temp.temperature, 5);
-
-    //Crear un objeto JSON
-    JsonDocument doc;
-    doc["fechaHoraLectura"] = isoTime;
-    doc["temp"] = temp.temperature;  // Asignar la temperatura
-    doc["humedad"] = humidity.relative_humidity;  // Asignar la humedad
-    doc["dioxidoDeCarbono"] = co2Filtrado;  // Asignar el valor de CO2
-    doc["idCaja"] = idCaja; */
-
-    // Generar valores aleatorios para pruebas
+// Generar valores aleatorios para pruebas
     float tempAleatoria = random(150, 350) / 10.0; // Temperatura entre 15.0 y 35.0 ºC
-    floar humedadAleatoria = random(300, 800) / 10.0; // Humedad entre 30.0 y 80.0 %
+    float humedadAleatoria = random(300, 800) / 10.0; // Humedad entre 30.0 y 80.0 %
     float co2Aleatorio = random(400, 2000); // CO2 entre 400 y 2000 ppm
     
     Serial.print("Temp: "); Serial.print(tempAleatoria);
@@ -139,17 +144,16 @@ void enviarLectura(){
     doc["dioxidoDeCarbono"] = co2Aleatorio;  // Asignar el valor de CO2 aleatorio
     doc["idCaja"] = idCaja;  // Asignar el ID de la caja
 
-
     //Serializar el JSON a una cadena
     String jsonData;
     serializeJson(doc, jsonData);
 
     Serial.println(jsonData);
     
-    
     //Verificar el estado de la conexión WiFi
     if (WiFi.status() == WL_CONNECTED) {
-      WiFiClient client;
+      WiFiClientSecure client;
+      client.setInsecure();
       HTTPClient http;
 
       http.begin(client, serverName);
