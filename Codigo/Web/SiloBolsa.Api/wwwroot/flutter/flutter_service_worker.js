d6f5cf6ac01fc30ca6a86786df80f8b95f20b7bb
marsaiz <48637197+marsaiz@users.noreply.github.com>
2025-11-04 14:29:41 -0300

    Flutter: actualizar build web con mas puntos en grafico y mas labels en eje X

--- Codigo/Web/SiloBolsa.Api/wwwroot/flutter/flutter_service_worker.js
+++ Codigo/Web/SiloBolsa.Api/wwwroot/flutter/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "907363f8e735bfc8245aff093fd33814",
+"flutter_bootstrap.js": "f34f666a4270b57117cec9fd57acc95d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "8e4b8635508711404492688569d2bf7d",
 "/": "8e4b8635508711404492688569d2bf7d",
-"main.dart.js": "28b315af8c9b6cde3ae2005eb8e3c8fc",
+"main.dart.js": "ad8358e739196a70787b51d8e41e003d",
 "manifest.json": "cace7594368df09c7e1b7d136cc24843",
 "version.json": "8113ccdc89583ae2fdbc18bc44e3202a"};
 // The application shell files that are downloaded before a service worker can
